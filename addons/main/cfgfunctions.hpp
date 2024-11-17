b7917d6fcd95600951e62c5c9082281cbb65ade9
SteamNimmersatt <nimmersatt@zebramail.eu>
2024-11-17 12:40:45 +0100

    Build - Fix hemtt build errors (#1)

--- addons/main/cfgfunctions.hpp
+++ addons/main/cfgfunctions.hpp
@@ -5,36 +5,36 @@ class AICommand
 	class Main
 	{
 		file = "\z\aicommand2\addons\main\functions";	
-		class initAICommand {description = ""};
-		class initAICommandClient {description = ""};
+		class initAICommand {description = "";};
+		class initAICommandClient {description = "";};
 	};
 	
 	class MapIcon
 	{
 		file = "\z\aicommand2\addons\main\functions\mapIcon";	
-		class createMapIcon {description = ""};
-		class drawMapIcon {description = ""};
-		class mapIconDefinitions {description = ""};
+		class createMapIcon {description = "";};
+		class drawMapIcon {description = "";};
+		class mapIconDefinitions {description = "";};
 	};
 	
 	class GroupData
 	{
 		file = "\z\aicommand2\addons\main\functions\groupData";
-		class addWaypoint {description = ""};	
-		class getAllWaypoints {description = ""};
-		class getWaypoint {description = ""};
-		class setWaypoint {description = ""};
-		class disableWaypoint {description = ""};
-		class disableAllWaypoints {description = ""};
-		class waitForWaypointGoCode { description = ""};
-		class sendWaypointGoCode { description = ""};
-		class setGroupColor {description = ""};
-		class getGroupColor {description = ""};
-		class getAllActiveWaypoints {description = ""};
-		class getGroupActions {description = ""};
-		class setGroupActions {description = ""};
-		class getGroupAssignedVehicles {description = ""};
-		class setGroupAssignedVehicles {description = ""};
+		class addWaypoint {description = "";};	
+		class getAllWaypoints {description = "";};
+		class getWaypoint {description = "";};
+		class setWaypoint {description = "";};
+		class disableWaypoint {description = "";};
+		class disableAllWaypoints {description = "";};
+		class waitForWaypointGoCode {description = "";};
+		class sendWaypointGoCode {description = "";};
+		class setGroupColor {description = "";};
+		class getGroupColor {description = "";};
+		class getAllActiveWaypoints {description = "";};
+		class getGroupActions {description = "";};
+		class setGroupActions {description = "";};
+		class getGroupAssignedVehicles {description = "";};
+		class setGroupAssignedVehicles {description = "";};
 	};
 
 	class CommandMenu
@@ -81,21 +81,21 @@ class AICommand
 	class CR
 	{
 		file = "\z\aicommand2\addons\main\functions\cr";
-		class createCommandRadio {description = ""};
-		class commandRadioSyncLoop {description = ""};
-		class enableCommandRadioTask {description = ""};
-		class takeCommandRadio {description = ""};
-		class setRadioOwner {description = ""};
-		class getRadioOwner {description = ""};
-		class getRadioOwners {description = ""};
-		class getOwnerRadio {description = ""};
-		class isRadioOwner {description = ""};
-		class taskDropRadio {description = ""};
-		class syncCommandRadioTasksLocal {description = ""};
-		class showCommandRadioMenu {description = ""};
-		class showCommandRadioMenuLocal {description = ""};
-		class setPublicVariable {description = ""};
-		class getPublicVariable {description = ""};
+		class createCommandRadio {description = "";};
+		class commandRadioSyncLoop {description = "";};
+		class enableCommandRadioTask {description = "";};
+		class takeCommandRadio {description = "";};
+		class setRadioOwner {description = "";};
+		class getRadioOwner {description = "";};
+		class getRadioOwners {description = "";};
+		class getOwnerRadio {description = "";};
+		class isRadioOwner {description = "";};
+		class taskDropRadio {description = "";};
+		class syncCommandRadioTasksLocal {description = "";};
+		class showCommandRadioMenu {description = "";};
+		class showCommandRadioMenuLocal {description = "";};
+		class setPublicVariable {description = "";};
+		class getPublicVariable {description = "";};
 	};
 	
 	class VehicleIcon
@@ -131,43 +131,43 @@ class AICommand
 	class InteractiveIconMapElement
 	{
 		file = "\z\aicommand2\addons\main\functions\mapElements\interactiveIcon";
-		class interactiveIconManager {description = ""};
-		class createInteractiveIcon {description = ""};
-		class drawInteractiveIcon {description = ""};
-		class handleInteractiveIconEvent {description = ""};
-		class interactiveIconEventHandler {description = ""};
-		class getInteractiveIconsByMapPosition {description = ""};
-		class removeInteractiveIcon {description = ""};
+		class interactiveIconManager {description = "";};
+		class createInteractiveIcon {description = "";};
+		class drawInteractiveIcon {description = "";};
+		class handleInteractiveIconEvent {description = "";};
+		class interactiveIconEventHandler {description = "";};
+		class getInteractiveIconsByMapPosition {description = "";};
+		class removeInteractiveIcon {description = "";};
 	};
 	
 	
 	class GroupControlMapElement
 	{
 		file = "\z\aicommand2\addons\main\functions\mapElements\groupControl";
-		class getGroupIconType {description = ""};
-		class getGroupControlIconSet {description = ""};
-		class getGroupControlWpIconSet {description = ""};
-		class createGroupControl {description = ""};
-		class drawGroupControl {description = ""};
-		class groupControlEventHandler {description = ""};
-		class groupControlWaypointEventHandler {description = ""};
-		class groupControlManager {description = ""};
-		class showGroupReport {description = ""};
-		class showGroupWaypointReport {description = ""};
-		class removeGroupControl {description = ""};
+		class getGroupIconType {description = "";};
+		class getGroupControlIconSet {description = "";};
+		class getGroupControlWpIconSet {description = "";};
+		class createGroupControl {description = "";};
+		class drawGroupControl {description = "";};
+		class groupControlEventHandler {description = "";};
+		class groupControlWaypointEventHandler {description = "";};
+		class groupControlManager {description = "";};
+		class showGroupReport {description = "";};
+		class showGroupWaypointReport {description = "";};
+		class removeGroupControl {description = "";};
 	};
 	
 	class CommandControlMapElement
 	{
 		file = "\z\aicommand2\addons\main\functions\mapElements\commandControl";
-		class commandControlManager {description = ""};
-		class createCommandControl {description = ""};
-		class drawCommandControl {description = ""};
-		class commandControlAddGroup {description = ""};
-		class commandControlRemoveGroup {description = ""};
-		class commandControlEventHandler {description = ""};
-		class showCommandControl {description = ""};
-		class sendGoCode {description = ""};
+		class commandControlManager {description = "";};
+		class createCommandControl {description = "";};
+		class drawCommandControl {description = "";};
+		class commandControlAddGroup {description = "";};
+		class commandControlRemoveGroup {description = "";};
+		class commandControlEventHandler {description = "";};
+		class showCommandControl {description = "";};
+		class sendGoCode {description = "";};
 	};
 	
 	class Util
